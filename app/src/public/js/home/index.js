50fb5a4acf12ce42c966168bcf3ec1035ac56304
LDH <leedoh0384@gmail.com>
2025-07-07 16:26:31 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/index.js
+++ app/src/public/js/home/index.js
@@ -1,2 +1,13 @@
 "use strict";
 
+const list = document.querySelector("#list"),
+loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", listAdd)
+
+function listAdd() {
+    const req = {
+        list: list.value
+    };
+    console.log(req);
+}
