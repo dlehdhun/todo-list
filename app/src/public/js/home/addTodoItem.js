67d57aeafc079487dd0f2ea0b1a7e0a45c0fc25b
LDH <leedoh0384@gmail.com>
2025-07-09 11:45:08 +0900

    할 일 추가 기능 모듈화

---
+++ app/src/public/js/home/addTodoItem.js
@@ -0,0 +1,37 @@
+"use strict";
+
+function add(req, todoBox, toDo) {
+    fetch("/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    })
+        .then((res) => res.json())
+        .then((res) => {
+            const html = `
+                <div class="todo-item">
+                <div class="todo-left">
+                    <input type="checkbox" class="todo-check" />
+                    <span class="todo-text">${res.toDo}</span>
+                </div>
+                <div class="todo-right">
+                    <button class="edit-btn">
+                    <img src="/images/home/pen-solid.svg" alt="수정" />
+                    </button>
+                    <button class="delete-btn">
+                    <img src="/images/home/trash-can.svg" alt="삭제" />
+                    </button>
+                </div>
+                </div>
+            `;
+            todoBox.insertAdjacentHTML("beforeend", html);
+            toDo.value = "";
+        })
+        .catch((err) => {
+            console.error("toDo 추가 중 w에러 발생");
+        });
+}
+
+export { add };
--- app/src/public/js/home/index.js
+++ app/src/public/js/home/index.js
@@ -1,92 +1,23 @@
 "use strict";
 
+import { add } from './addTodoItem.js';
 
 const toDo = document.querySelector("#todoInput"),
     loginBtn = document.querySelector("button"),
     todoBox = document.querySelector(".todo-box");
 
-
-
-// 엔터 누르면
-toDo.addEventListener('keydown', (event) => {
+function handleAdd(event) {
     const req = {
         toDo: toDo.value,
     };
+    if (event.key === "Enter" || event.type === "click") {
+        if (toDo.value === "") {
+            alert("값을 입력하세요!");
+        } else {
+            add(req, todoBox, toDo);
+        }
+    }
+}
 
-    if (event.key === 'Enter' && toDo.value != "") {
-         fetch("/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(req),
-        })
-            .then((res) => res.json())
-            .then((res) => {
-                const html = `
-                    <div class="todo-item">
-                    <div class="todo-left">
-                        <input type="checkbox" class="todo-check" />
-                        <span class="todo-text">${res.toDo}</span>
-                    </div>
-                    <div class="todo-right">
-                        <button class="edit-btn">
-                        <img src="/images/home/pen-solid.svg" alt="수정" />
-                        </button>
-                        <button class="delete-btn">
-                        <img src="/images/home/trash-can.svg" alt="삭제" />
-                        </button>
-                    </div>
-                    </div>
-                `
-                todoBox.insertAdjacentHTML("beforeend", html);
-                toDo.value = null;
-            })
-            .catch((err) => {
-                console.error("toDo추가 중 에러 발생");
-            });
-    } 
-    if (toDo.value === "" && event.key === 'Enter') alert("값을 입력하세요!");
-});
-
-
-// 버튼 누르면
-loginBtn.addEventListener("click", (event) => {
-    const req = {
-        toDo: toDo.value,
-    };
-    if (toDo.value !== '') {
-        fetch("/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(req),
-        })
-            .then((res) => res.json())
-            .then((res) => {
-                const html = `
-                    <div class="todo-item">
-                    <div class="todo-left">
-                        <input type="checkbox" class="todo-check" />
-                        <span class="todo-text">${res.toDo}</span>
-                    </div>
-                    <div class="todo-right">
-                        <button class="edit-btn">
-                        <img src="/images/home/pen-solid.svg" alt="수정" />
-                        </button>
-                        <button class="delete-btn">
-                        <img src="/images/home/trash-can.svg" alt="삭제" />
-                        </button>
-                    </div>
-                    </div>
-                `
-                todoBox.insertAdjacentHTML("beforeend", html);
-                toDo.value = null;
-            })
-            .catch((err) => {
-                console.error("toDo추가 중 에러 발생");
-            });
-    } 
-    if (toDo.value === "" && event.type === 'click') alert("값을 입력하세요!");
-});
+toDo.addEventListener("keydown", handleAdd);
+loginBtn.addEventListener("click", handleAdd);
